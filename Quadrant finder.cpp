c80fbe7c9f5d3e0ac85cc3ab657a7c30505808a7
Tejas151106 <gandlurisaitejasnvn@gmail.com>
2025-07-23 18:53:47 +0530

    Create Quadrant finder.cpp

---
+++ Quadrant finder.cpp
@@ -0,0 +1,37 @@
+//Gandluri Sai Tejas
+//24070123045
+//Batch: A2
+
+#include <iostream>
+using namespace std;
+int x,y;
+
+int main() {
+    cout<< "Value for x: ";
+    cin >> x;
+    cout<< "Value for y: ";
+    cin >> y;
+    
+    
+    if (x>0 && y>0){
+        cout<< "1st Quadrant" << "\n";
+    }
+    else if (x>0 && y<0){
+        cout<< "4th Quadrant" << "\n";
+    }
+    else if (x<0 && y<0){
+        cout<< "3rg Quadrant" << "\n";
+    }
+    else if (x<0 && y>0){
+        cout<< "2nd Quadrant" << "\n";
+    }
+     else if (x==0 && y==0){
+        cout<< "Origin" << "\n";
+    }    
+    else{
+        cout<< "Invalid input! "<< "\n";
+    }
+    
+    
+    return 0;
+}
